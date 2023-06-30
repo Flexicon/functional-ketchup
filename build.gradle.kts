f5933f5ac5a6449f5cd347e457c9437abab7217c
Mike Repec <mikerepec@gmail.com>
2023-06-30 12:06:06 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "dev.flexicon"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/dev/flexicon/functionalketchup/Option.kt
@@ -0,0 +1,18 @@
+package dev.flexicon.functionalketchup
+
+// Consider the Option type, which wraps a value.
+// As an Option, a value is only ever in one of two states:
+//      either it is something or it is nothing.
+
+
+// Provide a way to easily wrap a value as Option.
+
+
+// Provide a way to run any transformer function for Options which have Some value,
+// and simply return None for others.
+
+
+// Provide a simple way to get at the optional value inside an Option.
+
+
+// Provide a simple match against the state of the Option.
