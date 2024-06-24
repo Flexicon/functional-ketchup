25bfbb016b7175d467ced88ca29c086bac9d030c
Mike Repec <mikerepec@gmail.com>
2024-06-24 10:24:43 +0200

    feat: implement Option monad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
+import dev.flexicon.functionalketchup.Option
+import dev.flexicon.functionalketchup.flatMap
+import dev.flexicon.functionalketchup.fold
+import dev.flexicon.functionalketchup.get
+import dev.flexicon.functionalketchup.some
+
 fun main() {
     println("Hello ƛ🍅")
+
+    val result = getUserName()
+        .flatMap(::getLogsForUser)
+        .flatMap(::firstAndLast)
+
+    println("Result: ${result.get()}")
+
+    result.fold({ println("Your logs are: $it") }, { println("Failed to retrieve logs") })
+    val next = result.fold({ it }, { emptyList() })
+}
+
+fun getUserName(): Option<String> = some("Steve")
+
+fun getLogsForUser(username: String): Option<List<String>> = when (username) {
+    "John" -> some(listOf("hello", "world", "foo"))
+    else -> Option.None
+}
+
+fun <T> firstAndLast(list: List<T>): Option<List<T>> = when {
+    list.size < 2 -> Option.None
+    else -> some(listOf(list.first(), list.last()))
 }
--- src/main/kotlin/dev/flexicon/functionalketchup/Option.kt
+++ src/main/kotlin/dev/flexicon/functionalketchup/Option.kt
@@ -3,16 +3,27 @@ package dev.flexicon.functionalketchup
 // Consider the Option type, which wraps a value.
 // As an Option, a value is only ever in one of two states:
 //      either it is something or it is nothing.
+sealed interface Option<out T> {
+    data class Some<T>(val value: T) : Option<T>
 
+    object None : Option<Nothing>
+}
 
 // Provide a way to easily wrap a value as Option.
-
+fun <T> some(value: T): Option<T> = Option.Some(value)
 
 // Provide a way to run any transformer function for Options which have Some value,
 // and simply return None for others.
-
+fun <T, R> Option<T>.flatMap(f: (T) -> Option<R>): Option<R> = fold({ f(it) }, { Option.None })
 
 // Provide a simple way to get at the optional value inside an Option.
+fun <T> Option<T>.get(): T? = fold(onSome = { it }, onNone = { null })
 
+fun <T> Option<T>.getOrThrow(message: () -> String = { "Option is None" }): T =
+    fold({ it }, { throw IllegalStateException(message()) })
 
 // Provide a simple match against the state of the Option.
+fun <T, R> Option<T>.fold(onSome: (T) -> R, onNone: () -> R): R = when (this) {
+    is Option.None -> onNone()
+    is Option.Some -> onSome(value)
+}
